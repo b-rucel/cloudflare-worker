59e5c869e054a81eb4ddc4f2b30573259df5faa1
b-rucel <b-rucel@users.noreply.github.com>
2023-05-24 21:17:45 -0700

    fetching eth xen price

--- fetch-xenprices/src/index.js
+++ fetch-xenprices/src/index.js
@@ -10,6 +10,43 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
+
+		const dexscreener = 'https://api.dexscreener.com/latest/dex/pairs/'
+		const ethereum = dexscreener + 'ethereum/0x2a9d2ba41aba912316d16742f259412b681898db'
+    
+		const options = {
+			"headers": {
+			  "content-type": "application/json;charset=UTF-8",
+			},
+		};
+
+    async function parseResponse(response) {
+      const { headers } = response;
+      const contentType = headers.get("content-type") || "";   
+
+      if (contentType.includes("application/json")) {
+        const data = await response.json();
+
+        const result = {}
+        result[data.pair.chainId] = String(data.pair.priceUsd)
+        return result;
+      } else if (contentType.includes("application/text")) {
+        return response.text();
+      } else if (contentType.includes("text/html")) {
+        return response.text();
+      } else {
+        return response.text();
+      }
+    }
+		
+		const responses = await Promise.all([
+			fetch(ethereum, options),
+		])
+
+    const results = await Promise.all([
+      parseResponse(responses[0]),
+    ])
+
+    return new Response(JSON.stringify(results), options);
 	},
 };
