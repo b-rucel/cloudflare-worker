d7080b6bcbb7163d99724464dfe10c21f30e1b43
b-rucel <b-rucel@users.noreply.github.com>
2023-05-25 15:00:46 -0700

    wrappng up the source with 2 groupings of calls

--- fetch-xenprices/src/index.js
+++ fetch-xenprices/src/index.js
@@ -9,16 +9,26 @@
  */
 
 export default {
-	async fetch(request, env, ctx) {
+  async fetch(request, env, ctx) {
 
-		const dexscreener = 'https://api.dexscreener.com/latest/dex/pairs/'
-		const ethereum = dexscreener + 'ethereum/0x2a9d2ba41aba912316d16742f259412b681898db'
-    
-		const options = {
-			"headers": {
-			  "content-type": "application/json;charset=UTF-8",
-			},
-		};
+    const dexscreener = 'https://api.dexscreener.com/latest/dex/pairs/'
+    const ethereum = dexscreener + 'ethereum/0x2a9d2ba41aba912316d16742f259412b681898db'
+    const bsc = dexscreener + 'bsc/0xaaa77f0acdc01cbe71e74f177efd38697b5a7feb';
+    const polygon = dexscreener + 'polygon/0x97ffb2574257280e0fb2fa522345f0e81faae711';
+    const avalanche = dexscreener + 'avalanche/0x55e41d322a6da698137b4557431f2754d7e6ea5e';
+    const ethereumpow = dexscreener + 'ethereumpow/0x7fee6a68a09cf4592a04a3ae02cc6db0af6e6e34';
+    const moonbeam = dexscreener + 'moonbeam/0x43a094dc0fb493fae9955c32e88d6ef839c5df7e';
+    const evmos = dexscreener + 'evmos/0xf4ac0c589d8d0b196cf194026c646d00b1d40a76';
+    const fantom = dexscreener + 'fantom/0xab3738d7671a50e5c49d8c17b0e1f5051cbc4aad';
+    const dogechain = dexscreener + 'dogechain/0x77f1ef6d7afef2384db4768fa42aa3558823aab0';
+    const okc = dexscreener + 'okc/0x698e2966d6f38da1803e21fb04858f17bb1e5d3d';
+    const pulsechain = dexscreener + 'pulsechain/0x9194fe03e648e1220fa8267d699cef1a20a6ac88';
+
+    const options = {
+      "headers": {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    };
 
     async function parseResponse(response) {
       const { headers } = response;
@@ -38,15 +48,43 @@ export default {
         return response.text();
       }
     }
-		
-		const responses = await Promise.all([
-			fetch(ethereum, options),
-		])
 
-    const results = await Promise.all([
-      parseResponse(responses[0]),
+    const group_one_responses = await Promise.all([
+      fetch(ethereum, options),
+      fetch(bsc, options),
+      fetch(polygon, options),
+      fetch(avalanche, options),
+      fetch(ethereumpow, options),
+      fetch(moonbeam, options),
     ])
 
-    return new Response(JSON.stringify(results), options);
-	},
+    const group_one_results = await Promise.all([
+      parseResponse(group_one_responses[0]),
+      parseResponse(group_one_responses[1]),
+      parseResponse(group_one_responses[2]),
+      parseResponse(group_one_responses[3]),
+      parseResponse(group_one_responses[4]),
+      parseResponse(group_one_responses[5]),
+    ])
+
+    const group_two_responses = await Promise.all([
+      fetch(evmos, options),
+      fetch(fantom, options),
+      fetch(dogechain, options),
+      fetch(okc, options),
+      fetch(pulsechain, options),
+    ])
+
+    const group_two_results = await Promise.all([
+      parseResponse(group_two_responses[0]),
+      parseResponse(group_two_responses[1]),
+      parseResponse(group_two_responses[2]),
+      parseResponse(group_two_responses[3]),
+      parseResponse(group_two_responses[4]),
+    ])
+
+    const response = new Response(JSON.stringify(group_one_results.concat(group_two_results)), options);
+    response.headers.set("Cache-Control", "max-age=30");
+    return response;
+  },
 };
